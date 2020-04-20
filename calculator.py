948a7f90430e84d4ad6988e87362079c326e3427
Antoine Facerias <antoine.facerias@gmail.com>
2020-04-20 14:24:05 -0400

    Modification of the logic

--- calculator.py
+++ calculator.py
@@ -7,25 +7,34 @@ window.title("Calculator")
 count = 0
 
 #Functions to handle buttons
+def create_button(master, text, func, *args): #Create a button
+    if args:
+        return tk.Button(master = master, text = text, width = 5, height = 3, command = lambda: func(args))
+    else:
+        return tk.Button(master = master, text = text, width = 5, height = 3, command = lambda: func())
 
-def insert(button_value):
+def insert(button_value): #Insert the text value of the button clicked into the second field from the top
     ent_op.insert(tk.END, button_value)
 
-def clear():
+def clear(): #Clear the second field from the top
     ent_op.delete(0, tk.END)
 
-def clear_all():
+def clear_all(): #Clear the both entry fields
     clear()
     ent_result.delete(0, tk.END)
 
-def eq():
+def eq(): #Use a regex to determine the operation and return a result inserted in both entry fields
     value = ent_op.get()
+
     if value.isdigit():
         ent_result.insert(0, value)
+
     regex = re.compile('(\d*)([-+/*])(\d*)')
+
     m = regex.match(value)
     num1 = int(m.group(1))
     num2 = int(m.group(3))
+
     if m.group(2) == '+':
         result = num1 + num2
     elif m.group(2) == '-':
@@ -40,9 +49,9 @@ def eq():
     ent_op.insert(tk.END, result)
 
 #Create three frames for the window
-#First frame to display numbers entered and result of operation
-#Second frame to display a pad with digits and operators
-#Third frame to display an equal Button
+#frm_display to display numbers entered and result of operation
+#frm_pad to display a pad with digits and operators
+#frm_eq to display an equal Button
 
 frm_display = tk.Frame()
 frm_pad = tk.Frame()
@@ -59,35 +68,50 @@ ent_result.pack()
 ent_op.pack()
 
 #Create the main pad
-#Issue found: we can create all this buttons with three for loops
-#When it comes to get the text value from one button, since it has been created with the
-#variable, we end up getting the same last value that was created
-#Hence why I'm creating them manually and then looping through them
 
-btn_1 = tk.Button(master = frm_pad, text = '1', width = 5, height = 3, command = lambda: insert('1'))
-btn_2 = tk.Button(master = frm_pad, text = '2', width = 5, height = 3, command = lambda: insert('2'))
-btn_3 = tk.Button(master = frm_pad, text = '3', width = 5, height = 3, command = lambda: insert('3'))
-btn_4 = tk.Button(master = frm_pad, text = '4', width = 5, height = 3, command = lambda: insert('4'))
-btn_5 = tk.Button(master = frm_pad, text = '5', width = 5, height = 3, command = lambda: insert('5'))
-btn_6 = tk.Button(master = frm_pad, text = '6', width = 5, height = 3, command = lambda: insert('6'))
-btn_7 = tk.Button(master = frm_pad, text = '7', width = 5, height = 3, command = lambda: insert('7'))
-btn_8 = tk.Button(master = frm_pad, text = '8', width = 5, height = 3, command = lambda: insert('8'))
-btn_9 = tk.Button(master = frm_pad, text = '9', width = 5, height = 3, command = lambda: insert('9'))
+btn_1 = create_button(frm_pad, '1', insert, '1')
+btn_2 = create_button(frm_pad, '2', insert, '2')
+btn_3 = create_button(frm_pad, '3', insert, '3')
+btn_4 = create_button(frm_pad, '4', insert, '4')
+btn_5 = create_button(frm_pad, '5', insert, '5')
+btn_6 = create_button(frm_pad, '6', insert, '6')
+btn_7 = create_button(frm_pad, '7', insert, '7')
+btn_8 = create_button(frm_pad, '8', insert, '8')
+btn_9 = create_button(frm_pad, '9', insert, '9')
 
-first_part_pad = [btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9]
+btn_0 = create_button(frm_pad, '0', insert, '0')
+btn_AC = create_button(frm_pad, 'AC', clear_all)
+btn_C = create_button(frm_pad, 'C', clear)
 
-btn_0 = tk.Button(master = frm_pad, text = '0', width = 5, height = 3, command = lambda: insert('0'))
-btn_AC = tk.Button(master = frm_pad, text = 'AC', width = 5, height = 3, command = clear_all)
-btn_C = tk.Button(master = frm_pad, text = 'C', width = 5, height = 3, command = clear)
+btn_divide = create_button(frm_pad, '/', insert, '/')
+btn_multiply = create_button(frm_pad, '*', insert, '*')
+btn_minus = create_button(frm_pad, '-', insert, '-')
+btn_plus = create_button(frm_pad, '+', insert, '+')
 
-second_part_pad = [btn_0, btn_AC, btn_C]
+first_part_pad = [
+    btn_1,
+    btn_2,
+    btn_3,
+    btn_4,
+    btn_5,
+    btn_6,
+    btn_7,
+    btn_8,
+    btn_9
+]
 
-btn_divide = tk.Button(master = frm_pad, text = '/', width = 5, height = 3, command = lambda: insert('/'))
-btn_multiply = tk.Button(master = frm_pad, text = '*', width = 5, height = 3, command = lambda: insert('*'))
-btn_minus = tk.Button(master = frm_pad, text = '-', width = 5, height = 3, command = lambda: insert('-'))
-btn_plus = tk.Button(master = frm_pad, text = '+', width = 5, height = 3, command = lambda: insert('+'))
+second_part_pad = [
+    btn_0,
+    btn_AC,
+    btn_C
+]
 
-third_part_pad = [btn_divide, btn_multiply, btn_minus, btn_plus]
+third_part_pad = [
+    btn_divide,
+    btn_multiply,
+    btn_minus,
+    btn_plus
+]
 
 for i in range(3, 0, -1):
     for j in range(3):
